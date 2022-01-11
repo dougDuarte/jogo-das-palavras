a02ca3cab650bcbb51e88f75f506e3c94290800a
Milena Emmert <milena.emmert@outlook.com>
2022-01-11 15:32:18 -0300

    array words and choose random word

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const inputLetter3 = document.querySelector('data-letter3')
 const inputLetter4 = document.querySelector('data-letter4')
 const inputLetter5 = document.querySelector('data-letter5')
 const buttonRetry = document.querySelector('data-retry')
+
+var words = ["Amora", "Urutau", "Tigre", "Arara", "Coral", "Jambo", "Pasto", "Zinco", "Carpo", "Mioma"]
+
+var randomPosition = Math.floor(Math.random() * words.length)
