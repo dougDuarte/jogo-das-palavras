386dcb77508e882ed78c0c75beec73ccf1409b29
dougDuarte <douglasdduarte@outlook.com>
2022-01-11 02:52:58 -0300

    Primeiro commit.

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputLetter1 = document.querySelector('data-letter1')
+const inputLetter2 = document.querySelector('data-letter2')
+const inputLetter3 = document.querySelector('data-letter3')
+const inputLetter4 = document.querySelector('data-letter4')
+const inputLetter5 = document.querySelector('data-letter5')
+const buttonRetry = document.querySelector('data-retry')
