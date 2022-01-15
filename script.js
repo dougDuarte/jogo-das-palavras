043159c306b5879d73807ef9e184959a41396dc3
Milena Emmert <milena.emmert@outlook.com>
2022-01-15 16:03:58 -0300

    Permitir apenas letras e acentuação nos inputs

--- script.js
+++ script.js
@@ -38,11 +38,12 @@ function letterExists(element, index) {
 
 associateLetter()
 
-function checkInput(evt) {
-    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
-        ((evt.which) ? evt.which : 0));
-    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
-        (charCode < 97 || charCode > 122)) {
-        return false;
-    }
-}
+letterInputs.forEach((element) => {
+    element.addEventListener('keypress', function(e){
+        let keyCode = (e.keyCode ? e.keyCode : e.which)
+        if ((keyCode >= 0 && keyCode <= 64) || (keyCode >= 91 && keyCode <= 93) || (keyCode >= 95 && keyCode <= 96) || (keyCode >= 123 && keyCode <= 127)) {
+            e.preventDefault()
+        }
+    })
+})
+
