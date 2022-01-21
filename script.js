5e183c4c1eb30de411b12b5f28ad50e75257f79c
dougDuarte <douglasdduarte@outlook.com>
2022-01-21 02:23:15 -0300

    Atualização do visual e organização do código

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-const SQUARES = document.querySelectorAll('input.main-input')
-
-const WORDS = ["amora", "tigre", "arara", "coral", "jambo", "pasto", "zinco", "carpo", "mioma", "vinho", "banho"]
+const WORDS = ["amor", "figo", "casa", "pote", "açaí", "cano", "sono", "capa", "mimo", "nilo", "área", "ação"]
 const RANDOM_POSITION = Math.floor(Math.random() * WORDS.length)
 const CHOOSEN_WORD = WORDS[RANDOM_POSITION].toUpperCase()
 
-// DECLARAÇÃO DOS OBJETOS
-let slot1 = {rightLetter: fillSlot(0), userLetter: '', status: 'empty'}
-let slot2 = {rightLetter: fillSlot(1), userLetter: '', status: 'empty'}
-let slot3 = {rightLetter: fillSlot(2), userLetter: '', status: 'empty'}
-let slot4 = {rightLetter: fillSlot(3), userLetter: '', status: 'empty'}
-let slot5 = {rightLetter: fillSlot(4), userLetter: '', status: 'empty'}
-const SLOTS = [slot1, slot2, slot3, slot4, slot5]
 
-// ASSOCIA O INDEX DA PALAVRA SORTEADA AO PLACEHOLDER DO QUADRADO (v2.0)
-function fillSlot(index) {
-    SQUARES[index].placeholder = CHOOSEN_WORD[index]
-    return CHOOSEN_WORD[index]
+// CRIAÇÃO DOS INPUTS ----->
+const INPUTS = []
+
+function createInputs(count) {
+    const CONTAINER_INPUTS = document.querySelector('[data-container-inputs]')
+    for(let i = 0; i < count; i++) {
+        const INPUT_LETTER = document.createElement('input')
+        INPUT_LETTER.classList.add('input-letter')
+        INPUT_LETTER.setAttribute('type', 'text')
+        INPUT_LETTER.setAttribute('maxlength', '1')
+        INPUT_LETTER.placeholder = CHOOSEN_WORD[i]
+        CONTAINER_INPUTS.appendChild(INPUT_LETTER)
+        INPUTS.push(INPUT_LETTER)
+    }
 }
 
-// TRIGGERS DO EVENTO DE INPUT (v2.0)
-SQUARES.forEach((element, index) => {
+createInputs(4)
+
+
+// DECLARAÇÃO DOS OBJETOS ----->
+const SLOTS = []
+
+function assignToSquare () {
+    for(let i = 0; i < INPUTS.length; i++) {
+        SLOTS.push({rightLetter: CHOOSEN_WORD[i], userLetter: '', status: 'empty'})
+    }
+    showLog(SLOTS)
+}
+
+assignToSquare()
+
+
+// TRIGGERS DO EVENTO DE INPUT ----->
+INPUTS.forEach((element, index) => {
     element.addEventListener('input', () => {
         element.value = element.value.toUpperCase()
         SLOTS[index].userLetter = element.value
         testLetter()
-        showLog()
+        showLog(SLOTS)
         insertColor()
     })
 })
 
-// LÓGICA PRINCIPAL DOS ESTADOS
+
+// LÓGICA DOS ESTADOS ----->
 function testLetter() {
     for(let i = 0; i < SLOTS.length; i++) {
 
@@ -46,45 +64,38 @@ function testLetter() {
                 if(SLOTS[a].rightLetter === SLOTS[i].userLetter && SLOTS[a].userLetter !== SLOTS[a].rightLetter && SLOTS[a] !== SLOTS[i]) {
                     match = true
                 }
-                match ? SLOTS[i].status = 'wrongPlace' : SLOTS[i].status = 'wrong'
+                match ? SLOTS[i].status = 'misplaced' : SLOTS[i].status = 'wrong'
             }
         }
     }
 }
 
-// LÓGICA DAS CORES
+
+// LÓGICA DAS CORES ----->
 function insertColor() {
     for(i in SLOTS) {
         switch(SLOTS[i].status) {
             case 'right':
-            SQUARES[i].style.outlineColor = 'green'
+            INPUTS[i].setAttribute('class', 'input-letter --right')
             break
 
-            case 'wrongPlace':
-            SQUARES[i].style.outlineColor = 'orange'
+            case 'misplaced':
+            INPUTS[i].setAttribute('class', 'input-letter --misplaced')
             break
 
             case 'wrong':
-            SQUARES[i].style.outlineColor = 'red'
+            INPUTS[i].setAttribute('class', 'input-letter --wrong')
             break
 
             default:
-            SQUARES[i].style.outlineColor = '#00000000'
+            INPUTS[i].setAttribute('class', 'input-letter')
         }
     }
 }
 
 
-// DEBUG
-function showLog() {
-    console.clear()
-    for(i of SLOTS) {
-        console.log(i)
-    }
-}
-
-// FILTRA OS INPUTS PERMITIDOS NOS SQUARES
-SQUARES.forEach((element) => {
+// FILTRA OS INPUTS PERMITIDOS NOS INPUTS
+INPUTS.forEach((element) => {
     element.addEventListener('keypress', function(e){
         let keyCode = (e.keyCode ? e.keyCode : e.which)
         if ((keyCode >= 0 && keyCode <= 64) || (keyCode >= 91 && keyCode <= 93) || (keyCode >= 95 && keyCode <= 96) || (keyCode >= 123 && keyCode <= 127)) {
@@ -92,3 +103,12 @@ SQUARES.forEach((element) => {
         }
     })
 })
+
+
+// DEBUG
+function showLog(element) {
+    console.clear()
+    for(i of element) {
+        console.log(i)
+    }
+}
